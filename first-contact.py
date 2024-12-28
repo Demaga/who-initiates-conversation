96493a63dba8e769736de5ad73a230a6e496735d
Bohdan Kholodenko <bohdan.kholodenko@gmail.com>
2024-12-28 11:35:46 +0300

    Clean up code; change pie chart title to English

--- first-contact.py
+++ first-contact.py
@@ -6,25 +6,13 @@ f = open("result.json")
 data = json.load(f)
 
 data = data["messages"]
-# print(data)
 
 df = pd.DataFrame.from_records(data)
-print(df.head)
-
-for col in df.columns:
-    print(col)
-
 df["date"] = df["date"].transform(lambda x: x.split('T')[0])
 
-print(df.head)
-
 df = df.groupby('date').first()
-
-print(df.head)
-
 df = df.groupby("from").count()
 df = df.reset_index()
-print(df)
 
-fig = px.pie(df, values='id', names='from', title='Первое сообщение за день')
+fig = px.pie(df, values='id', names='from', title='First to message')
 fig.show()
